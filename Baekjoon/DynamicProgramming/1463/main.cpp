adf05daa20ee2a89dab01885695a8c6523bb9c07
Unknown <valotam@kmou.ac.kr>
2018-06-26 22:11:04 +0900

    [Debugging] #1463: Out of time

--- Baekjoon/DynamicProgramming/1463/main.cpp
+++ Baekjoon/DynamicProgramming/1463/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <deque>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,21 +15,74 @@ public:
     finder(const int num_in) : _num_input(num_in)
     {
         if (num_in < 1 && num_in > 1000000) {
-            cerr << "Input number (" << num_in << ") is out of range!" << endl;
+            cout << "Input number (" << num_in << ") is out of range!" << endl;
         }
         
         _count_operated = 0;
     }
     ~finder() { }
-    int find();
+    void cal();
     int get_count();
 };
 
-int finder::find()
+void finder::cal()
 {
-    int result;
-    
-    return result;
+    _count_operated = 0;
+
+    vector<int> list_searched;
+    list_searched.push_back(1);
+
+    int ii_start = 0;
+    int ii_last = 1;
+    bool is_found = false;
+
+    while(ii_start < list_searched.size()){
+        int size_of_level = 0;
+                
+        for(int ii = ii_start; ii < ii_last; ii++)
+        {
+            if (list_searched[ii] == _num_input)
+            {
+                is_found = true;
+                break;
+            }
+            else
+            {
+                int value = list_searched[ii] + 1;
+                auto it = find(list_searched.begin(), list_searched.end(), value);
+                if (it == list_searched.end() && value <= _num_input)
+                {
+                    list_searched.push_back(value);
+                    size_of_level++;
+                }
+                value = list_searched[ii] * 2;
+                it = find(list_searched.begin(), list_searched.end(), value);
+                if (it == list_searched.end() && value <= _num_input)
+                {
+                    list_searched.push_back(value);
+                    size_of_level++;
+                }
+                value = list_searched[ii] * 3;
+                it = find(list_searched.begin(), list_searched.end(), value);
+                if (it == list_searched.end() && value <= _num_input)
+                {
+                    list_searched.push_back(value);
+                    size_of_level++;
+                }
+            }
+        }
+
+        if (is_found)
+        {
+            break;
+        }
+        else
+        {
+            ii_start = ii_last;
+            ii_last += size_of_level;
+            _count_operated++;
+        }
+    }
 }
 
 int finder::get_count()
@@ -38,8 +92,10 @@ int finder::get_count()
 
 int main(int argc, char const *argv[])
 {
-    finder a(10);
-    a.find();
+    int in;
+    cin >> in;
+    finder a(in);
+    a.cal();
     cout << a.get_count() << endl;
     return 0;
 }
